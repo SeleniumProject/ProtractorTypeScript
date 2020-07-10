070513989cd1a838ff34ef42799ffdf97dd7588e
SeleniumProject <seleniumden@gmail.com>
2020-07-10 16:38:29 +0530

    First Commit

---
+++ Ptotractor_TS_Async-cucumber/testData/Cust.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    "siteurl" : "https://www.collectionsetc.com/",
+    "customers" :{
+        "firstname" : "John",
+        "lastname" : "Carry",
+        "postalcode" : "87654"
+    }
+}
---
+++ Ptotractor_TS_Async-cucumber/testData/appGlobals.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    appUrl : 'https://www.collectionsetc.com/',
+    customer : 
+    [
+      {
+        firstName : 'Qav',
+        lastName : 'Box',
+        postCode : '65789'
+      },
+      {
+        firstName : 'John',
+        lastName : 'Doe',
+        postCode : '123456'
+      }
+    ]
+}
